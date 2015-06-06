22863029a14f70f2a27f4700038c4bbc650a7b3f
itjesse <jesse@itjesse.cn>
2015-06-06 19:56:00 +0800

    新增铺面及list接口

---
+++ routes/beatmaps.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var parser = require('osu-parser');
+
+/* GET home page. */
+router.get('/list', function(req, res) {
+    var dirList = fs.readdirSync('./public/beatmaps');
+    var list = [];
+    var result = {};
+
+    dirList.forEach(function(item){
+        var beatmap = {};
+        if(item.indexOf('.DS_Store') == -1){
+            beatmap.path = item;
+            var dirList = fs.readdirSync('./public/beatmaps/' + item);
+            beatmap.list = [];
+            dirList.forEach(function(item2){
+                if(item2.indexOf('.osu') != -1){
+                    beatmap.list.push(item2);
+                }
+            });
+            list.push(beatmap);
+        }
+    });
+
+    result.beatmaps = list;
+    res.json(result);
+});
+
+module.exports = router;
