8e93d8e4bcf56214f73afb0bf531f539745e302d
itjesse <jesse@itjesse.cn>
2015-06-06 15:30:17 +0800

    愉快的钦定方案

--- public/js/draw_iidx.js
+++ public/js/draw_iidx.js
@@ -2,8 +2,11 @@ var height = window.innerHeight;
 var width = window.innerWidth;
 var basicPanelWidth = 480;
 
-var renderer = PIXI.autoDetectRenderer(width, height, {backgroundColor : 0x1099bb});
-document.body.appendChild(renderer.view);
+var gameRenderer = PIXI.autoDetectRenderer(width, height, {
+    backgroundColor: 0x1099bb,
+    view: document.getElementById('game'),
+    autoResize: true
+});
 
 // create the root of the scene graph
 var stage = new PIXI.Container();
@@ -14,22 +17,29 @@ basicPanel.beginFill(0x646464, 0.6);
 basicPanel.lineStyle(3, 0x000, 1);
 basicPanel.drawRect((width - basicPanelWidth) / 2, -3, basicPanelWidth, height + 6);
 
-var texture = PIXI.Texture.fromVideo('./beatmaps/26044 Wada Kouji - Butter-Fly (TV Edit)/Digimon Adventure OP.mp4');
-var videoSprite = new PIXI.Sprite(texture);
-
-videoSprite.width = renderer.width;
-videoSprite.height = renderer.height;
-
-stage.addChild(videoSprite);
 stage.addChild(basicPanel);
 
 // start animating
-animate();
+animateGame();
 
-function animate() {
+var bgm = document.getElementById('bgm');
 
-    requestAnimationFrame(animate);
+bgm.oncanplay = function() {
+    var texture = PIXI.Texture.fromVideo('./beatmaps/26044 Wada Kouji - Butter-Fly (TV Edit)/Digimon Adventure OP.mp4');
+    var videoSprite = new PIXI.Sprite(texture);
+
+    videoSprite.width = gameRenderer.width;
+    videoSprite.height = gameRenderer.height;
+
+    stage.addChild(videoSprite);
+    stage.setChildIndex(videoSprite, 0);
+    this.play();
+};
+
+function animateGame() {
+
+    requestAnimationFrame(animateGame);
 
     // render the root container
-    renderer.render(stage);
+    gameRenderer.render(stage);
 }
