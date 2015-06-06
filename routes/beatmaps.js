9c1ff2e1a15c6975699b4484fb6521ea2f1acf45
itjesse <jesse@itjesse.cn>
2015-06-06 20:50:49 +0800

    添加铺面输出

--- routes/beatmaps.js
+++ routes/beatmaps.js
@@ -9,14 +9,14 @@ router.get('/list', function(req, res) {
     var list = [];
     var result = {};
 
-    dirList.forEach(function(item){
+    dirList.forEach(function(item) {
         var beatmap = {};
-        if(item.indexOf('.DS_Store') == -1){
+        if (item.indexOf('.DS_Store') == -1) {
             beatmap.path = item;
             var dirList = fs.readdirSync('./public/beatmaps/' + item);
             beatmap.list = [];
-            dirList.forEach(function(item2){
-                if(item2.indexOf('.osu') != -1){
+            dirList.forEach(function(item2) {
+                if (item2.indexOf('.osu') != -1) {
                     beatmap.list.push(item2);
                 }
             });
@@ -27,5 +27,13 @@ router.get('/list', function(req, res) {
     result.beatmaps = list;
     res.json(result);
 });
+
+router.get('/json/:path/:osu', function(req, res) {
+    var path = './public/beatmaps/' + req.params.path + '/' + req.params.osu;
+    parser.parseFile(path, function(err, beatmap) {
+        res.json(beatmap);
+    });
+});
+
 
 module.exports = router;
