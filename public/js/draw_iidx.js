b21036f74bd3ed5f1c665cb84026d8c08fc11d4b
itjesse <jesse@itjesse.cn>
2015-06-06 16:42:28 +0800

    基本绘制了一半

--- public/js/draw_iidx.js
+++ public/js/draw_iidx.js
@@ -1,9 +1,17 @@
 var height = window.innerHeight;
 var width = window.innerWidth;
-var basicPanelWidth = 480;
+var basicPanelWidth = 500;
+var basicPanelLineWidth = 5;
+var markerPanelWidth = 440;
+var markerPanelTextureRadius = 30;
+var markerPanelTextureWidth = markerPanelWidth - 60;
+var markerPanelTextureLineWidth = 3;
+var healPanelWidth = (basicPanelWidth - markerPanelWidth) / 2;
+var buttonPanelHeight = 100;
 
 var gameRenderer = PIXI.autoDetectRenderer(width, height, {
-    backgroundColor: 0x1099bb,
+    backgroundColor: 0xFFFFFF,
+    transparent: true,
     view: document.getElementById('game'),
     autoResize: true
 });
@@ -13,9 +21,37 @@ var stage = new PIXI.Container();
 
 // draw main panel, (x, y, width, height)
 var basicPanel = new PIXI.Graphics();
-basicPanel.beginFill(0x646464, 0.6);
-basicPanel.lineStyle(3, 0x000, 1);
-basicPanel.drawRect((width - basicPanelWidth) / 2, -3, basicPanelWidth, height + 6);
+//basicPanel background
+basicPanel.beginFill(0x000, 0.5);
+basicPanel.lineStyle(basicPanelLineWidth, 0x000, 1);
+basicPanel.drawRect((width - basicPanelWidth) / 2, -basicPanelLineWidth, basicPanelWidth, height + basicPanelLineWidth * 2);
+//markerPanel
+basicPanel.beginFill(0x000, 0.7);
+basicPanel.lineStyle(1, 0x000, 1);
+basicPanel.drawRect((width - basicPanelWidth) / 2 + healPanelWidth, 0, markerPanelWidth, height);
+// //markerPanel texture
+// basicPanel.beginFill(0xFFF, 0);
+// basicPanel.lineStyle(3, 0xEF6868, 1);
+// basicPanel.drawRoundedRect((width - markerPanelTextureWidth) / 2, - markerPanelTextureRadius - markerPanelTextureLineWidth, markerPanelTextureWidth, height - buttonPanelHeight, markerPanelTextureRadius);
+basicPanel.beginFill(0xFFF, 0);
+basicPanel.lineStyle(3, 0xEF6868, 1);
+basicPanel.drawPolygon([
+    (width - markerPanelTextureWidth) / 2,
+    - markerPanelTextureRadius - markerPanelTextureLineWidth,
+    (width - markerPanelTextureWidth) / 2 + markerPanelTextureWidth,
+    - markerPanelTextureRadius - markerPanelTextureLineWidth,
+    (width - markerPanelTextureWidth) / 2 + markerPanelTextureWidth,
+    height - buttonPanelHeight - 50,
+    (width - markerPanelTextureWidth) / 2 + markerPanelTextureWidth - 20,
+    height - buttonPanelHeight,
+    (width - markerPanelTextureWidth) / 2 + 20,
+    height - buttonPanelHeight,
+    (width - markerPanelTextureWidth) / 2,
+    height - buttonPanelHeight - 50
+]);
+basicPanel.beginFill(0xEF6868, 1);
+basicPanel.moveTo((width - markerPanelTextureWidth) / 2 + 10, height - buttonPanelHeight - 25);
+basicPanel.lineTo((width - markerPanelTextureWidth) / 2 + markerPanelTextureWidth - 10, height - buttonPanelHeight - 25);
 
 stage.addChild(basicPanel);
 
@@ -35,6 +71,11 @@ bgm.oncanplay = function() {
     stage.setChildIndex(videoSprite, 0);
     this.play();
 };
+
+// bgm.onended = function() {
+//     stage.removeChild(videoSprite);
+//     texture.destory();
+// };
 
 function animateGame() {
 
